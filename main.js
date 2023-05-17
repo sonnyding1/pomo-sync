659284accd2b3837618da75c76721ef5135f9ab4
Sonny Ding <sonnyding20@gmail.com>
2023-05-17 10:05:05 -0700

    add state text

--- main.js
+++ main.js
@@ -1,66 +1,12 @@
 const workTimeInput = document.getElementById('work-time');
 const restTimeInput = document.getElementById('rest-time');
 const controlBtn = document.getElementById('start');
-const timerDisplay = document.getElementById('timer');
 const countdownNumber = document.getElementById('countdown-number');
+const state = document.getElementById('state');
 const circle = document.querySelector('circle');
 
-// let timer; 
 
-// controlBtn.addEventListener('click', function() {
-//     const workTime = workTimeInput.value * 60; // convert minutes to seconds
-//     const restTime = restTimeInput.value * 60; // convert minutes to seconds
-//     // controlBtn.disabled = true; // disable the start button
-    
-//     if (controlBtn.classList.contains('start')) {
-//         controlBtn.innerText = 'Stop';
-//         startTimer(workTime, restTime);
-//     } else {
-//         controlBtn.innerText = 'Start';
-//         controlBtn.disabled = true;
-//     }
-//     controlBtn.classList.toggle('start');
-    
-// });
-
-// function startTimer(workTime, restTime) {
-//     let currentTime = 0;
-//     let state = 1;
-//     circle.style.strokeDashoffset = 301;
-
-//     timer = setInterval(function() {
-
-//         if (!controlBtn.classList.contains('start')) {
-//             clearInterval(timer);
-//             controlBtn.disabled = false;
-//         }
-
-//         currentTime = currentTime + 1;
-        
-//         const minutes = Math.floor(currentTime / 60);
-//         const seconds = currentTime % 60;
-//         circle.style.strokeDashoffset = 301 - (currentTime / (state === 1 ? workTime : restTime)) * 301;
-//         countdownNumber.innerText = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-//         timerDisplay.innerText = `Current state is: ${state > 0 ? "work" : "rest"}`;
-
-//         // work time reached
-//         if (currentTime >= workTime && state === 1) {
-//             alert('Work time is over! Rest time begins.');
-//             currentTime = 0;
-//             state = 0;
-//         }
-        
-//         // rest time reached
-//         if (currentTime >= restTime && state === 0) {
-//             clearInterval(timer);
-//             alert('Rest time is over!');
-//             controlBtn.disabled = false;
-//         }
-//     }, 1000);
-// }
-
-
-// Variables to keep track of timer state
+// Variables
 let isRunning = false;
 let isStudy = false;
 let timerInterval;
@@ -90,6 +36,7 @@ function startTimer() {
         startTime = Date.now();
         timerInterval = setInterval(updateTimer, 1000);
         controlBtn.innerText = 'Stop';
+        state.textContent = `${isStudy ? 'studying...' : 'resting...'}`  // update state
     }
 }
 
@@ -98,6 +45,7 @@ function stopTimer() {
         isRunning = false;
         clearInterval(timerInterval);
         controlBtn.innerText = 'Start';
+        state.textContent = ''  // clear state
     }
 }
 
@@ -110,8 +58,8 @@ function updateTimer() {
     // update display
     countdownNumber.textContent = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     // update progress bar
-    circle.style.strokeDashoffset = 301 - (elapsedTime / 1000 / (isStudy === true ? workTime : restTime)) * 301;
-    console.log(isStudy);
+    circle.style.strokeDashoffset = 301 - (elapsedTime / 1000 / (isStudy ? workTime : restTime)) * 301;
+    
 
 }
 
